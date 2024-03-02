3cc137efa9adb7eef0109be4edec98ea3495f6e6
Ambroise Bareux <abareux@student.42mulhouse.fr>
2024-03-02 12:45:49 +0100

    puttype finished ? need to find test

--- puttype.c
+++ puttype.c
@@ -6,11 +6,13 @@
 /*   By: abareux <abareux@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/24 17:01:38 by abareux           #+#    #+#             */
-/*   Updated: 2024/02/26 12:53:46 by abareux          ###   ########.fr       */
+/*   Updated: 2024/03/02 12:37:45 by abareux          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "puttype.h"
+
+#include <stdio.h>
 #include <elf.h>
 
 void	puttype_64(Elf64_Sym symbol, Elf64_Ehdr file_eheader, void *file_data)
@@ -19,23 +21,44 @@ void	puttype_64(Elf64_Sym symbol, Elf64_Ehdr file_eheader, void *file_data)
 	unsigned char	bind = ELF64_ST_BIND(symbol.st_info);
 //	unsigned char	info = ELF64_ST_INFO(bind, type);
 	Elf64_Shdr		*linked_section_header = file_data + file_eheader.e_shoff + file_eheader.e_shentsize * symbol.st_shndx;
-	Elf64_Shdr		*str_section_header = file_data + file_eheader.e_shoff + file_eheader.e_shentsize * file_eheader.e_shstrndx;
+//	Elf64_Shdr		*str_section_header = file_data + file_eheader.e_shoff + file_eheader.e_shentsize * file_eheader.e_shstrndx;
+	
+	if (symbol.st_shndx == SHN_ABS)
+		write(1, "A", 1);
 
-	if (linked_section_header->sh_name > 0)
+	else if (linked_section_header->sh_type == SHT_NOBITS && bind == STB_GLOBAL)
+		write(1, "B", 1);
+	else if (linked_section_header->sh_type == SHT_NOBITS && bind == STB_LOCAL)
+		write(1, "b", 1);
+	
+	else if (bind == STB_WEAK && linked_section_header->sh_flags != 0)
+		write(1, "W", 1);
+	else if (bind == STB_WEAK)
+		write(1, "w", 1);
+
+	else if (symbol.st_shndx == SHN_UNDEF)
+		write(1, "U", 1);
+
+	else if (!(linked_section_header->sh_flags & SHF_WRITE))
 	{
-		if (ft_strncmp(file_data + str_section_header->sh_offset + linked_section_header->sh_name, ".bss") == 0 && bind == STB_GLOBAL)
-			write(1, "B", 1);
-		else if (ft_strncmp(file_data + str_section_header->sh_offset + linked_section_header->sh_name, ".bss") == 0 && bind == STB_LOCAL)
-			write(1, "b", 1);
-		else if (ft_strncmp(file_data + str_section_header->sh_offset + linked_section_header->sh_name, ".data") == 0 && bind == STB_GLOBAL)
-			write(1, "D", 1);
-		else if (ft_strncmp(file_data + str_section_header->sh_offset + linked_section_header->sh_name, ".data") == 0 && bind == STB_LOCAL)
-			write(1, "d", 1);
-		else if (ft_strncmp(file_data + str_section_header->sh_offset + linked_section_header->sh_name, ".stab") == 0)
-			write(1, "N", 1);
-		else if (ft_strncmp(file_data + str_section_header->sh_offset + linked_section_header->sh_name, ".stabstr") == 0)
-			write(1, "N", 1);
-		else
-			write(1, "?", 1);
+		if (linked_section_header->sh_flags & SHF_ALLOC && linked_section_header->sh_flags & SHF_EXECINSTR && bind == STB_GLOBAL)
+			write(1, "T", 1);
+		else if (linked_section_header->sh_flags & SHF_ALLOC && linked_section_header->sh_flags & SHF_EXECINSTR && bind == STB_LOCAL)
+			write(1, "t", 1);
+		else if (bind == STB_GLOBAL)
+			write(1, "R", 1);
+		else if (bind == STB_LOCAL)
+			write(1, "r", 1);
 	}
+	
+	else if (symbol.st_shndx && bind == STB_GLOBAL)
+		write(1, "D", 1);
+	else if (symbol.st_shndx && bind == STB_LOCAL)
+		write(1, "d", 1);
+
+
+	else if (bind == STB_LOCAL)
+		write(1, "local", 5);
+	else if (bind == STB_GLOBAL)
+		write(1, "global", 6);
 }
