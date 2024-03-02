23437c6dab3537c7d5525bb369ead60a994b6291
Ambroise Bareux <abareux@student.42mulhouse.fr>
2024-03-02 12:58:51 +0100

    32 bit version

--- puttype.h
+++ puttype.h
@@ -6,7 +6,7 @@
 /*   By: abareux <abareux@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/24 17:03:10 by abareux           #+#    #+#             */
-/*   Updated: 2024/02/26 10:57:13 by abareux          ###   ########.fr       */
+/*   Updated: 2024/03/02 12:51:05 by abareux          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,4 +15,5 @@
 
 #include "utils.h"
 
+void	puttype_32(Elf32_Sym symbole, Elf32_Ehdr file_eheader, void *file_data);
 void	puttype_64(Elf64_Sym symbole, Elf64_Ehdr file_eheader, void *file_data);
--- show_symbol.c
+++ show_symbol.c
@@ -6,7 +6,7 @@
 /*   By: abareux <abareux@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/22 13:46:36 by abareux           #+#    #+#             */
-/*   Updated: 2024/02/26 10:56:41 by abareux          ###   ########.fr       */
+/*   Updated: 2024/03/02 12:58:12 by abareux          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,12 +20,42 @@
 
 int	show_symbol_32(void *file_data)
 {
-	Elf32_Ehdr *file_eheader = file_data;
-	Elf32_Phdr *file_pheader = file_data + file_eheader->e_phoff;
-	Elf32_Shdr *file_sheader = file_data + file_eheader->e_shoff;
-	(void) file_pheader;
-	(void) file_sheader;
-	return (1);
+	Elf32_Ehdr	*file_eheader = file_data;
+	Elf32_Shdr	*file_sheader;
+	Elf32_Shdr	*file_symtab_header = NULL;
+	Elf32_Shdr	*file_str_symtab_header = NULL;
+
+	for (int cursor = 1; cursor < file_eheader->e_shnum; cursor++)
+	{
+		file_sheader = file_data + file_eheader->e_shoff + file_eheader->e_shentsize * cursor;
+		if (file_sheader->sh_type == SHT_SYMTAB)
+			file_symtab_header = file_sheader;
+	}
+
+	if (!file_symtab_header)
+		return (1);
+
+	file_str_symtab_header = file_data + file_eheader->e_shoff + file_eheader->e_shentsize * file_symtab_header->sh_link;
+
+	Elf32_Sym	*symtab = file_data + file_symtab_header->sh_offset;
+
+	symtab++;
+
+	for (int cursor = 1; cursor < (int) file_symtab_header->sh_size / 24; cursor++)
+	{
+		char *name = file_data + file_str_symtab_header->sh_offset + symtab->st_name;
+		if (!((name[ft_strlen(name) - 1] == 'o' || name[ft_strlen(name) - 1] == 'c') && name[ft_strlen(name) - 2] == '.') && name[0])
+		{
+			ft_putptr((void *) (unsigned long) symtab->st_value);
+			write(1, " ", 1);
+			puttype_32(*symtab, *file_eheader, file_data);
+			write(1, " ", 1);
+			write(1, name, ft_strlen(name));
+			write(1, "\n", 1);
+		}
+		symtab++;
+	}
+	return (0);
 }
 
 int	show_symbol_64(void *file_data)
@@ -56,7 +86,7 @@ int	show_symbol_64(void *file_data)
 		char *name = file_data + file_str_symtab_header->sh_offset + symtab->st_name;
 		if (!((name[ft_strlen(name) - 1] == 'o' || name[ft_strlen(name) - 1] == 'c') && name[ft_strlen(name) - 2] == '.') && name[0])
 		{
-			ft_putptr((void *) symtab->st_value);
+			ft_putptr((void *) (unsigned long) symtab->st_value);
 			write(1, " ", 1);
 			puttype_64(*symtab, *file_eheader, file_data);
 			write(1, " ", 1);
